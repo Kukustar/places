c73de042e9de14ab59ed73f82c108f950c46eb7e
abuser <anton@tifia.com>
2021-07-15 14:51:22 +0200

    4.6.3 - create sight details screen

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:places/domain/sight.dart';
+import 'package:places/ui/screen/sight_details.dart';
 
 
 class SightCard extends StatelessWidget {
@@ -8,89 +9,95 @@ class SightCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          height: 188,
-          width: 328,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                  image: NetworkImage(sight.url.toString()),
-                  fit: BoxFit.cover)
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          child: Container(
-            height: 88,
+    return InkWell(
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SightDetailsScreen(sight))
+      ),
+      child: Stack(
+        children: [
+          Container(
+            height: 188,
             width: 328,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(10),
-                    bottomLeft: Radius.circular(10)),
-                color: Colors.grey.shade100
-            ),
-            child: Padding(
-              padding: EdgeInsets.only(
-                  left: 16.0,
-                  top: 16.0
-              ),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      sight.name,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500
-                      ),
-                    ),
-                    RichText(
-                      maxLines: 3,
-                      text: TextSpan(
-                        text: sight.details.toString(),
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.grey
-                        )
-                      ),
-                    )
-                  ],
-              ),
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                    image: NetworkImage(sight.url.toString()),
+                    fit: BoxFit.cover)
             ),
           ),
-        ),
-        Positioned(
-            top: 0,
+          Positioned(
+            bottom: 0,
             child: Container(
+              height: 88,
               width: 328,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)),
+                  color: Colors.grey.shade100
+              ),
               child: Padding(
-                padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text(
-                      sight.type.toString(),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
+                padding: EdgeInsets.only(
+                    left: 16.0,
+                    top: 16.0
+                ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        sight.name,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      RichText(
+                        maxLines: 3,
+                        text: TextSpan(
+                          text: sight.details.toString(),
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.grey
+                          )
+                        ),
                       )
-                    ),
-                    Container(
-                      color: Colors.grey.shade100,
-                      width: 20,
-                      height: 18,
-                    )
-                  ],
+                    ],
                 ),
               ),
             ),
-        )
-      ],
+          ),
+          Positioned(
+              top: 0,
+              child: Container(
+                width: 328,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        sight.type.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        )
+                      ),
+                      Container(
+                        color: Colors.grey.shade100,
+                        width: 20,
+                        height: 18,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+          )
+        ],
+      ),
     );
   }
 }
